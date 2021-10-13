873bd7ddf6de5516cd4726416d8d3684040ff440
Shynianup <shynianup@gmail.com>
2021-10-13 12:52:55 +0800

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. You will see few exciting features terraform here...
+  Welcome to ${PREFIX}'s app. You will see few exciting features of terraform here...
   <!-- END -->
 
   </div>
